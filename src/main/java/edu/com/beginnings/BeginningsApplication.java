4f10fe917bc3e58ff3b108811c68398aaa73c30b
Alexisfllv <alexis-fl-live@hotmail.com>
2025-03-19 12:59:14 -0500

    base

---
+++ src/main/java/edu/com/beginnings/BeginningsApplication.java
@@ -0,0 +1,13 @@
+package edu.com.beginnings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeginningsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeginningsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/com/beginnings/BeginningsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.com.beginnings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeginningsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
