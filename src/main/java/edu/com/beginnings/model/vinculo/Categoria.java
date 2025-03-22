27cd268a5aa7df4c05c94059ca62c67f6ff2a449
Alexisfllv <alexis-fl-live@hotmail.com>
2025-03-22 11:43:57 -0500

    creacion de tablas /vinculo

---
+++ src/main/java/edu/com/beginnings/model/vinculo/Categoria.java
@@ -0,0 +1,23 @@
+package edu.com.beginnings.model.vinculo;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "categorias")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "categoria_id")
+    private Integer id;
+
+    @Column(name = "categoria_nombre")
+    private String nombre;
+}
---
+++ src/main/java/edu/com/beginnings/model/vinculo/Comida.java
@@ -0,0 +1,46 @@
+package edu.com.beginnings.model.vinculo;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "comidas")
+public class Comida {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comida_id")
+    private Integer id;
+
+    @Column(name = "comida_nombre")
+    private String nombre;
+
+    @Column(name = "producto_cantidad", nullable = false)
+    private Integer cantidad;
+
+    @Column(name = "producto_peso", precision = 10, scale = 3, nullable = false)
+    private BigDecimal peso;
+
+    @Column(name = "producto_fecha_inicio", nullable = true)
+    private LocalDateTime fechaInicio;
+
+    @Column(name = "producto_fecha_fin", nullable = true)
+    private LocalDateTime fechaFin;
+
+    //fk
+    @ManyToOne
+    @JoinColumn(name = "categoria_id", nullable = false)
+    private Categoria categoria;
+
+
+}
