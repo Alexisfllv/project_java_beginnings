66394339bac3582fcb2e5db1af8bc64bc684576c
Alexisfllv <alexis-fl-live@hotmail.com>
2025-03-24 22:39:09 -0500

    agregando documetancion swagger/springdoc

--- src/main/java/edu/com/beginnings/controller/vinculo/ComidaController.java
+++ src/main/java/edu/com/beginnings/controller/vinculo/ComidaController.java
@@ -7,6 +7,8 @@ import edu.com.beginnings.dto.record.vinculo.ComidaUpdateDTO;
 import edu.com.beginnings.paginador.PaginaRespuestaDTO;
 import edu.com.beginnings.service.vinculo.ComidaService;
 import edu.com.beginnings.mensaje.RespuestaDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Usuarios", description = "Operaciones relacionadas con usuarios")
 @RestController
 @RequestMapping("/vinculo/comidas")
 @RequiredArgsConstructor
@@ -27,6 +30,7 @@ public class ComidaController {
 
 
     //listar comidas con detallas
+    @Operation(summary = "Obtener todos los usuarios", description = "Devuelve la lista completa de usuarios")
     @GetMapping("/listar")
     public ResponseEntity<List<ComidaResponseDTO>> listarComidas() {
         return ResponseEntity.status(200).body(comidaService.listarComidas());
